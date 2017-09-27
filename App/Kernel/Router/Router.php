c61ee09ff354240c4a48ed8b4a5384f30fb5f25d
Christophe Daloz - De Los Rios <christophedlr@gmail.com>
2017-09-27 09:18:56 +0200

    - Add autoload in composer.json - Add phpcs and phpunit configuration

---
+++ App/Kernel/Router/Router.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: chris
+ * Date: 27/09/2017
+ * Time: 07:44
+ */
+
+namespace App\Kernel\Router;
+
+
+class Router
+{
+
+}
---
+++ Tests/App/RouterTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: chris
+ * Date: 27/09/2017
+ * Time: 07:42
+ */
+
+namespace AppTest;
+
+
+class RouterTest
+{
+
+}
