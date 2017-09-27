a269fc9ae51f1ef3b606fb0895ea9c34983650bd
Christophe Daloz - De Los Rios <christophedlr@gmail.com>
2017-09-27 09:19:31 +0200

    - Create router lib - Create test router lib

--- App/Kernel/Router/Router.php
+++ App/Kernel/Router/Router.php
@@ -1,15 +1,107 @@
 <?php
 /**
- * Created by IntelliJ IDEA.
- * User: chris
- * Date: 27/09/2017
- * Time: 07:44
+ *
+ *  Copyright Christophe Daloz - De Los Rios, 2017
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the “Software”), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  The Software is provided “as is”, without warranty of any kind, express or
+ *  implied, including but not limited to the warranties of merchantability,
+ *  fitness for a particular purpose and noninfringement. In no event shall the
+ *  authors or copyright holders be liable for any claim, damages or other liability,
+ *  whether in an action of contract, tort or otherwise, arising from, out of or in
+ *  connection with the software or the use or other dealings in the Software.
  */
 
 namespace App\Kernel\Router;
 
+use Symfony\Component\Routing\Generator\UrlGenerator;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
 
+/**
+ * Class Router
+ * @package App\Kernel\Router
+ * @author Christophe Daloz - De Los Rios <christophedlr@gmail.com>
+ * @version 1.0
+ */
 class Router
 {
+    private $routes;
+    private $context;
+    private $urlGenerator;
 
+    public function __construct()
+    {
+        $host = ( isset($_SERVER['HTTP_HOST']) ) ?: 'localhost';
+        $scheme = ( !empty($_SERVER['HTTPS']) ) ? 'https' : 'http';
+        $queryString = ( isset($_SERVER['QUERY_STRING']) ) ?: '/';
+
+        $this->routes = new RouteCollection();
+        $this->context = new RequestContext(
+            '',
+            'GET',
+            $host,
+            $scheme,
+            80,
+            443,
+            $_SERVER['PATH'],
+            $queryString
+        );
+        $this->urlGenerator = new UrlGenerator($this->routes, $this->context);
+    }
+
+    /**
+     * Search data of route indicate by path
+     * @param string $path
+     * @return array
+     */
+    public function match(string $path): array
+    {
+        $matcher = new UrlMatcher($this->routes, $this->context);
+
+        return $matcher->match($path);
+    }
+
+    /**
+     * Get route by name
+     * @param string $name
+     * @return null|Route
+     */
+    public function get(string $name)
+    {
+        return $this->routes->get($name);
+    }
+
+    /**
+     * Add new route
+     * @param string $routeName
+     * @param Route $route
+     */
+    public function addRoute(string $routeName, Route $route): void
+    {
+        $this->routes->add($routeName, $route);
+    }
+
+    /**
+     * Generate a URL
+     * @param string $routeName Name of route
+     * @param array $parameters List of parameters for route
+     * @param int $type 0 for absolute, 1 for relative
+     * @return string
+     */
+    public function generateUri(string $routeName, array $parameters = [], int $type = 0): string
+    {
+        return $this->urlGenerator->generate($routeName, $parameters, $type);
+    }
 }
--- Tests/App/RouterTest.php
+++ Tests/App/RouterTest.php
@@ -1,15 +1,96 @@
 <?php
 /**
- * Created by IntelliJ IDEA.
- * User: chris
- * Date: 27/09/2017
- * Time: 07:42
+ *
+ *  Copyright Christophe Daloz - De Los Rios, 2017
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the “Software”), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  The Software is provided “as is”, without warranty of any kind, express or
+ *  implied, including but not limited to the warranties of merchantability,
+ *  fitness for a particular purpose and noninfringement. In no event shall the
+ *  authors or copyright holders be liable for any claim, damages or other liability,
+ *  whether in an action of contract, tort or otherwise, arising from, out of or in
+ *  connection with the software or the use or other dealings in the Software.
  */
 
 namespace AppTest;
 
+use App\Kernel\Router\Router;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Route;
 
-class RouterTest
+class RouterTest extends TestCase
 {
+    /**
+     * @var Router
+     */
+    private $router;
 
+    public function setUp()
+    {
+        $this->router = new Router();
+    }
+
+    public function testMatchRoute()
+    {
+        $this->setTestRoute();
+
+        $array = $this->router->match('/');
+        $this->assertNotEquals(0, $array);
+    }
+
+    public function testRouteNotFound()
+    {
+        $this->setTestRoute();
+
+        $this->expectException(ResourceNotFoundException::class);
+        $array = $this->router->match('/test');
+    }
+
+    public function testRelativeUri()
+    {
+        $this->setTestRoute('/test');
+
+        $uri = $this->router->generateUri('index', [], 1);
+        $this->assertEquals('/test', $uri);
+    }
+
+    public function testAbsoluteUri()
+    {
+        $this->setTestRoute('/test');
+
+        $uri = $this->router->generateUri('index', [], 0);
+        $this->assertEquals('http://localhost/test', $uri);
+    }
+
+    public function testMatchByName()
+    {
+        $this->setTestRoute('/coucou', 'bouh');
+
+        $route = $this->router->get('bouh');
+        $this->assertInstanceOf(Route::class, $route);
+    }
+
+    public function testRouteNotFoundByName()
+    {
+        $this->setTestRoute('/coucou', 'coucou');
+
+        $route = $this->router->get('bouh');
+        $this->assertNotInstanceOf(Route::class, $route);
+    }
+
+    private function setTestRoute(string $path = '/', string $name = 'index')
+    {
+        $route = new Route($path, ['_controller' => 'App:Default:index']);
+        $this->router->addRoute($name, $route);
+    }
 }
