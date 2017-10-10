716bb3802d87739058eae1f6d872a3b19f2b3096
Christophe Daloz - De Los Rios <christophedlr@gmail.com>
2017-10-10 07:51:55 +0200

    - Add: SuperGlobals manager - Add: $_POST manager - Add: $_SESSION manager

---
+++ App/Kernel/SuperGlobals/PostGlobal.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ *  Copyright Christophe Daloz - De Los Rios, 2017
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the “Software”), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  The Software is provided “as is”, without warranty of any kind, express or
+ *  implied, including but not limited to the warranties of merchantability,
+ *  fitness for a particular purpose and noninfringement. In no event shall the
+ *  authors or copyright holders be liable for any claim, damages or other liability,
+ *  whether in an action of contract, tort or otherwise, arising from, out of or in
+ *  connection with the software or the use or other dealings in the Software.
+ */
+
+namespace App\Kernel\SuperGlobals;
+
+/**
+ * Manage $_POST SuperGlobal
+ * @package App\Kernel\SuperGlobals
+ */
+class PostGlobal
+{
+    /**
+     * GET $_POST key
+     * @param mixed $key
+     * @return mixed|null
+     */
+    public function get($key)
+    {
+        if ( isset($_POST[$key]) ) {
+            return $_POST[$key];
+        }
+
+        return null;
+    }
+
+    /**
+     * @param $key
+     * @return bool
+     */
+    public function is_null($key): bool
+    {
+        return is_null($_POST[$key]);
+    }
+
+    /**
+     * @param $key
+     * @return bool
+     */
+    public function is_int($key): bool
+    {
+        return is_int($_POST[$key]);
+    }
+
+    /**
+     * @param $key
+     * @return bool
+     */
+    public function is_bool($key): bool
+    {
+        return is_bool($_POST[$key]);
+    }
+
+    /**
+     * @param $key
+     * @return bool
+     */
+    public function is_string($key): bool
+    {
+        return is_string($_POST[$key]);
+    }
+
+    /**
+ * @param $key
+ * @return bool
+ */
+    public function is_numeric($key): bool
+    {
+        return is_numeric($_POST[$key]);
+    }
+
+    /**
+     * @param $key
+     * @return bool
+     */
+    public function is_float($key): bool
+    {
+        return is_float($_POST[$key]);
+    }
+}
