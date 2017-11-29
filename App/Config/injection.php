7907885da7632b8faa09757fae0f2cf28bae48e2
Christophe Daloz - De Los Rios <christophedlr@gmail.com>
2017-11-29 19:03:00 +0100

    - Add: Modules list
    TODO: replace QBuilder by Doctrine

--- App/Config/injection.php
+++ App/Config/injection.php
@@ -2,23 +2,55 @@
 
 use App\Kernel\Lang\LangManager;
 use App\Kernel\QBuilder\EntityManager;
+use App\Kernel\QBuilder\QBuilder;
+use App\Kernel\QBuilder\SelectInterface;
 use App\Kernel\SuperGlobals\SuperGlobals;
 
 use function DI\get;
 use function DI\object;
+use Modules\News\Controller\NewsController;
 
 $json = json_decode(file_get_contents('App/Config/parameters.json'), true);
+$modules = [];
+$pdo = new \PDO(
+    'mysql:dbname='.$json['database']['dbname'].';host='.
+    $json['database']['host'].';port='.$json['database']['port'],
+    $json['database']['user'],
+    $json['database']['password']
+);
 
-return [
+$base = [
     'parameters' => $json,
-    \PDO::class => \DI\object()->constructor(
+    'database' => $json['database'],
+    \PDO::class => object()->constructor(
         'mysql:dbname='.$json['database']['dbname'].';host='.
             $json['database']['host'].';port='.$json['database']['port'],
         $json['database']['user'],
         $json['database']['password'],
         []
     ),
+    QBuilder::class => object()->constructor(get(\PDO::class)),
     SuperGlobals::class => object(),
-    EntityManager::class => object()->constructor(get(\PDO::class)),
+    //EntityManager::class => object()->constructor(get(\PDO::class)),
     LangManager::class => object()->constructor($json['lang']),
 ];
+
+$builder = new QBuilder($pdo);
+$select = $builder->select('modules');
+$select
+    ->addField('id')
+    ->addField('autolaunch')
+    ->addField('state')
+    ->addField('name')
+    ->addField('classname')
+    ->addField('author')
+    ->addField('version')
+    ->addWhere('state', 1);
+$select->execute();
+$results = $select->fetchAll();
+
+foreach ($results as $result) {
+    $modules[$result['name']] = object($result['classname']);
+}
+
+return array_merge($base, $modules);
